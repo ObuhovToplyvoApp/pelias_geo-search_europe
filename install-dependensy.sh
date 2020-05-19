bcb07016da9e9104be46dfd2150fc317f05b8acf
Серега Обухов <obuhov.s@toplyvo.app>
2020-05-19 12:23:04 +0300

    Chmod and new json file

--- install-dependensy.sh
+++
@@ -1,8 +0,0 @@
-# change directory to the where you would like to install Pelias
-# cd /path/to/install
-
-# clone this repository
-git clone https://github.com/pelias/docker.git && cd docker
-
-# install pelias script
-ln -s "$(pwd)/pelias" /usr/local/bin/pelias
